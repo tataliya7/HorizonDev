7f0013d8953cbbcb3a6e3b5119f3785b7fbf147d
tataliya7 <81619976+tataliya7@users.noreply.github.com>
2024-06-01 20:51:45 +0800

    dev

---
+++ Source/HorizonEngine/Source/Engine/Components/Components.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Engine/Components/LightComponent.h"
+#include "Engine/Components/SkyAtmosphereComponent.h"
+#include "Engine/Components/SceneHierarchyComponent.h"
+#include "Engine/Components/TransformComponent.h"
+#include "Engine/Components/NameComponent.h"
