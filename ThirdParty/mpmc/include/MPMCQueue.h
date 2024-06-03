4d7d16dbcdeacf4f00b214cbfa317db29b40e681
tataliya7 <81619976+tataliya7@users.noreply.github.com>
2024-06-03 13:12:50 +0800

    dev

--- ThirdParty/mpmc/include/MPMCQueue.h
+++ ThirdParty/mpmc/include/MPMCQueue.h
@@ -111,9 +111,9 @@ namespace rigtorp {
         template <typename T, typename Allocator = AlignedAllocator<Slot<T>>>
         class Queue {
         private:
-            static_assert(std::is_nothrow_copy_assignable<T>::value ||
-                std::is_nothrow_move_assignable<T>::value,
-                "T must be nothrow copy or move assignable");
+            //static_assert(std::is_nothrow_copy_assignable<T>::value ||
+            //    std::is_nothrow_move_assignable<T>::value,
+            //    "T must be nothrow copy or move assignable");
 
             static_assert(std::is_nothrow_destructible<T>::value,
                 "T must be nothrow destructible");
