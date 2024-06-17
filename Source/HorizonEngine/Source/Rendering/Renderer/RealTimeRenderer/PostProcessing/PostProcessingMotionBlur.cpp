07520898d68aa2985c32ccb3d9f32471efd98ece
tataliya7 <81619976+tataliya7@users.noreply.github.com>
2024-06-18 01:48:25 +0800

    Motion Blur

--- Source/HorizonEngine/Source/Rendering/Renderer/RealTimeRenderer/PostProcessing/PostProcessingMotionBlur.cpp
+++ Source/HorizonEngine/Source/Rendering/Renderer/RealTimeRenderer/PostProcessing/PostProcessingMotionBlur.cpp
@@ -12,6 +12,23 @@ namespace Horizon
         RenderGraph& renderGraph,
         const SceneView& view)
     {
+        Extent2D velocityTileCount = Extent2D();
+
+        RenderGraphTextureDesc velocityAndDepthTextureDesc = RenderGraphTextureDesc::Create2D(
+            renderResolution.width,
+            renderResolution.height,
+            RenderBackendTextureFormat::R11G11B10Float,
+            RenderBackendTextureCreateFlags::ShaderResource | RenderBackendTextureCreateFlags::UnorderedAccess);
+        RenderGraphTextureHandle velocityAndDepthTexture = renderGraph.CreateTexture(velocityAndDepthTextureDesc, "MotionBlurVelocityAndDepthTexture");
+
+        RenderGraphTextureDesc velocityTileTextureDesc = RenderGraphTextureDesc::Create2D(
+            velocityTileCount.width,
+            velocityTileCount.height,
+            RenderBackendTextureFormat::R16G16Float,
+            RenderBackendTextureCreateFlags::ShaderResource | RenderBackendTextureCreateFlags::UnorderedAccess | RenderBackendTextureCreateFlags::RenderTarget);
+        RenderGraphTextureHandle velocityTileTexture = renderGraph.CreateTexture(velocityTileTextureDesc, "MotionBlurVelocityTileTexture");
+        RenderGraphTextureHandle dilatedVelocityTileTexture = renderGraph.CreateTexture(velocityTileTextureDesc, "MotionBlurDilatedVelocityTileTexture");
+
         return RenderGraphTextureHandle::Null;
     }
 }
