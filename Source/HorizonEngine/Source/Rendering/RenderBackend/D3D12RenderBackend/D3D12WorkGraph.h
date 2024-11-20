276c2d5a68ffcf4a6c427a3b7c727236219481dc
tataliya7 <81619976+tataliya7@users.noreply.github.com>
2024-11-20 23:04:24 +0800

    Work graph
    (cherry picked from commit 56604adae16bcecd1f13072addc6f1783ef423b5)

---
+++ Source/HorizonEngine/Source/Rendering/RenderBackend/D3D12RenderBackend/D3D12WorkGraph.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Horizon
+{
+    /**
+     * Each individual program is something that can be bound as a unit on the GPU for execution.
+     * Consisting of a grouping of shaders and/or fixed function operations that are launched as logical grouping on the GPU.
+     */
+    struct Direct3D12Program
+    {
+
+    };
+}
